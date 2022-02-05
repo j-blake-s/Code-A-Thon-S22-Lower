9ea713a5d3774dac39f40d06d2745049579f5c01
LexingtonWhalen <whalenlex@gmail.com>
2022-02-05 12:00:21 -0500

    done (i think)

--- PalindromePlaza/soln.py
+++ PalindromePlaza/soln.py
@@ -47,19 +47,63 @@ def test_palindrome(in_str):
 		
 		return 1
 
+# function to test each row, column, and diagonal
+def test_mat(mat):
+	# square mat
+	cols = len(mat[0])
+	# counter for palindromes
+	pal_count = 0
+	# first we do rows
+	for n,i in enumerate(mat):
+		pal_count += test_palindrome(i)
+	# test columns
+	for i in range(cols):
+		col = "".join([sub[i] for sub in mat])
+		pal_count += test_palindrome(col)
+	
+	# test diagonals (only one direction)
+	for i in range(0,cols):
+		curr_row = 0
+		curr_col = i
+		curr_str = ""
+		for j in range(i +1):
+			curr_str += mat[curr_row][curr_col]
+			curr_row +=1
+			curr_col -=1
+		pal_count += test_palindrome(curr_str)
+	# go the other way
+	for i in range(0,cols-1):
+		curr_row = cols - 1
+		curr_col = cols - i -1
+		curr_str = ""
+		for j in range(i+1):
+			curr_str += mat[curr_row][curr_col]
+			curr_row -=1
+			curr_col +=1
+
+		pal_count += test_palindrome(curr_str)
+	
+	return pal_count
+
 
 		
 
+mat0 = [
+	'abcd',
+	'efgh',
+	'ijkl',
+	'mnop',
+]
+mat1 = [
+	'aba',
+	'dcd',
+	'aba'
+]
+print(test_mat(mat1))
 
 
 
 
-
-
-print(test_palindrome("hannah"))
-print(test_palindrome("car"))
-print(test_palindrome('abcd'))
-print(test_palindrome('abacaba'))
 
 # with open("in.txt") as f:
 # 	for line in f:
